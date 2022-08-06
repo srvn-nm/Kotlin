c1d1f4357bc5a02894fdfa0606f32b89b5e7f6bd
Sarvin Nami <srvn0nm@gmail.com>
2022-08-06 16:17:59 +0430

    compiling the project

---
+++ salaryCounter/src/Main.kt
@@ -0,0 +1,5 @@
+fun main() {
+   var FatherPerMonth = 2000
+   var MotherPerMonth = 2000
+   println("My father's salary per year is: "+(FatherPerMonth*12)+"\nMy mother's salary per year is: "+(MotherPerMonth*12)+"\nMy Family's salary per year is: "+((MotherPerMonth*12)+(FatherPerMonth*12)))
+}
