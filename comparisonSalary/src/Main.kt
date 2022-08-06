ee3adac1de0f129a7e2fd1afd24f7293ea4147a6
Sarvin Nami <srvn0nm@gmail.com>
2022-08-06 16:39:04 +0430

    conditions on if and else

--- comparisonSalary/src/Main.kt
+++ comparisonSalary/src/Main.kt
@@ -1,3 +1,13 @@
 fun main(){
-
+    var FatherPerMonthLastyear = 1000
+    var FatherPerMonthThisYear = 2000
+    if (FatherPerMonthThisYear>FatherPerMonthLastyear){
+        println("This year after that promotion, we can buy more ^-^")
+    }
+    else if(FatherPerMonthLastyear==FatherPerMonthThisYear){
+        println("Nothing changed through a year -_-")
+    }
+    else{
+        println("Oh no! What a tragedy T^T")
+    }
 }
