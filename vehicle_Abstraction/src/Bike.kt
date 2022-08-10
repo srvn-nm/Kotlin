62264fbc168054595f609cc8ce701cba9094eb95
Sarvin Nami <srvn0nm@gmail.com>
2022-08-10 19:53:47 +0430

    adding old files

---
+++ vehicle_Abstraction/src/Bike.kt
@@ -0,0 +1,16 @@
+class Bike(price: Int, number: String, color: String, capacity: Int, helmet: Boolean) : Vehicle(price, number, color, capacity) {
+
+    private var helmet:Boolean
+
+    init{
+        this.helmet = helmet
+    }
+
+    fun ride(): String {
+        return "Happy riding this beautiful bike! ^-^"
+    }
+
+    override fun energy(): String {
+        return "${super.energy()} your muscles! You Strong *-*"
+    }
+}
---
+++ vehicle_Abstraction/src/Costumer.kt
@@ -0,0 +1,13 @@
+class Costumer constructor(bankBalance:Int){
+    var bankBalance: Int
+    private lateinit var choice:Vehicle
+
+    init{
+        this.bankBalance = bankBalance
+    }
+
+    fun buy(choice:Vehicle):String{
+        this.choice = choice
+        return "Congratulations! You successfully buy ${this.choice}."
+    }
+}
